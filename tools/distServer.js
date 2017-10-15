554022572e8b8e4af30099235fdb5fa54c777e0b
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-10-15 20:01:54 +0300

    mster

--- tools/distServer.js
+++ tools/distServer.js
@@ -4,7 +4,8 @@ const path = require('path');
 const express = require('express');
 /* eslint-disable no-console */
 
-const port = 8080;
+// const port = 8080;
+const port = process.env.PORT;
 const app = express();
 
 app.use(express.static('dist'));
@@ -13,11 +14,11 @@ app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../dist/index.html'));
 });
 
-app.listen(process.env.PORT || 5000)
-// app.listen(port, function(err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     open(`http://localhost:${port}`);
-//   }
-// });
+// app.listen(process.env.PORT || 5000)
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
