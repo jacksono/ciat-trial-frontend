1799226532bba6ff47b1c09eed73371086339678
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-10-26 20:45:38 +0300

    Solve jquery

--- tools/distServer.js
+++ tools/distServer.js
@@ -5,7 +5,7 @@ const express = require('express');
 /* eslint-disable no-console */
 
 // const port = 8080;
-// const port = process.env.PORT;
+const port = process.env.PORT;
 const app = express();
 
 app.use(express.static('dist'));
@@ -14,7 +14,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 5000)
 // app.listen(port, function(err) {
 //   if (err) {
 //     console.log(err);
