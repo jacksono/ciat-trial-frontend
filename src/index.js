3cb66e04cd60b3a437d42fb4f26abe8319d5c623
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-10-26 11:27:21 +0300

    Center all buttons

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import './styles/jquery.min.js';
+import './styles/bootstrap-select.min.css';
+import './styles/bootstrap.min.js';
+import './styles/bootstrap-select.min.js';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import './styles/jquery.min.js';
-// import './styles/bootstrap-select.min.css';
-// import './styles/bootstrap.min.js';
-// import './styles/bootstrap-select.min.js';
 
 
 render(
